98640def24024be8aee68e9032c95f2c988525e0
Caleb Trepowski <calebtrepowski@gmail.com>
2022-03-30 00:24:34 -0400

    api Temporarily deleted database connection

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -7,16 +7,16 @@ import { AppService } from './app.service';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(<string>process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      autoLoadEntities: true,
-      synchronize: true //false para production!!
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: process.env.POSTGRES_HOST,
+    //   port: parseInt(<string>process.env.POSTGRES_PORT),
+    //   username: process.env.POSTGRES_USER,
+    //   password: process.env.POSTGRES_PASSWORD,
+    //   database: process.env.POSTGRES_DATABASE,
+    //   autoLoadEntities: true,
+    //   synchronize: true //false para production!!
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
