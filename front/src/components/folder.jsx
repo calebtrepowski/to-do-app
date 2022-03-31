82c8e0cfeef8041c78152e2bc03644485978b170
Caleb Trepowski <calebtrepowski@gmail.com>
2022-03-31 08:30:26 -0400

    front
    added styling
    added folderlist with folders components

--- front/src/components/folder.jsx
+++ front/src/components/folder.jsx
@@ -1,4 +1,13 @@
-const Folder = () => {
-  return <></>;
+const Folder = ({ id, name }) => {
+  const handleClick = () => {
+    console.log(id);
+  };
+
+  return (
+    <div className="folder-item" onClick={handleClick}>
+      <h2>{name}</h2>
+      <i className="fa fa-arrow-right" aria-hidden="true"></i>
+    </div>
+  );
 };
 export default Folder;
